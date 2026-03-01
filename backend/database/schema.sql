356899aec7fce906a98b0c578bc784d6344cab22
cliaw <cliaw@student.unimelb.edu.au>
2026-03-01 23:38:35 +0800

    Firebase connection

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,24 +1,24 @@
 CREATE DATABASE IF NOT EXISTS my_app_db;
 USE my_app_db;
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
+    username VARCHAR(100) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE posts (
+CREATE TABLE IF NOT EXISTS posts (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    content
+    content TEXT NOT NULL,
     caption VARCHAR(300) NOT NULL,
     likes INT NOT NULL,
     retweets INT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE post_media (
+CREATE TABLE IF NOT EXISTS post_media (
     id INT AUTO_INCREMENT PRIMARY KEY,
     post_id INT NOT NULL,
     media_url TEXT NOT NULL,
@@ -28,16 +28,37 @@ CREATE TABLE post_media (
     FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE
 );
 
-CREATE TABLE follows (
+CREATE TABLE IF NOT EXISTS followers (
     follower_id INT NOT NULL,
     following_id INT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-
+	INDEX idx_follower_id (follower_id),
+    INDEX idx_following_id (following_id),
+    
     PRIMARY KEY (follower_id, following_id),
 
     FOREIGN KEY (follower_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY (following_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
-CREATE INDEX idx_follower_id ON follows(follower_id);
-CREATE INDEX idx_following_id ON follows(following_id);
+CREATE TABLE IF NOT EXISTS post_likes (
+    user_id INT NOT NULL,
+    post_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	INDEX idx_post_id_likes (post_id),
+    PRIMARY KEY (user_id, post_id),
+
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS post_reshares (
+    user_id INT NOT NULL,
+    post_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	INDEX idx_post_id_reshare (post_id),
+    PRIMARY KEY (user_id, post_id),
+
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE
+);
